22c54da1d2deea121656dbb3bcd2f5cd1406b4c2
vsratheesh <vsratheesh95@gmail.com>
2016-07-21 15:02:02 +0530

    handle the zero key

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 	$(".key").click(function(){
-		var number = $(this).text();
-		$('#preview').append(number);
-		console.log(this.innerText);
+		var key = $(this).text();
+		if (key =='0'){
+			if($('#preview').html() !='0'){
 
+				$('#preview').append(key);
+			}
+
+			} else {
+
+			$('#preview').append(key);
+		}
 	});
 
-})
+});
