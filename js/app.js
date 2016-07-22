4438e6af972dead6156fcbcdb075b1e6c07bcffd
vsratheesh <vsratheesh95@gmail.com>
2016-07-22 12:49:09 +0530

    handling operations

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
 	$(".key").click(function(){
-		var key = $(this).text();
+		var key = $(this).html();
 		if (key =='0'){
 			if($('#preview').html() !='0'){
-
 				$('#preview').append(key);
 			}
 
-			} else if(key == 'CLR'){
-				var newPreview = $('#preview').html().slice(0,-1);
-				$('#preview').html(newPreview);
-			}else {
+		} else if(key == 'CLR'){
 
+			var newPreview = $('#preview').html().slice(0,-1);
+			$('#preview').html(newPreview);
+
+		} else if(['+','-','*','/'].indexOf(key) != -1){
+
+			var lastNum = $('#preview').html().slice(-1);
+
+			if(['+','-','*','/'].indexOf(lastNum) != -1){
+
+				var newPreview = $('#preview').html().slice(0,-1);
+			    $('#preview').html(newPreview);
+			}
+			if($('#preview').html() != '' || (key == '-')){
+
+			$('#preview').append(key);
+			}
+
+		} else {
 			$('#preview').append(key);
 		}
 	});
-
-});
+});  
